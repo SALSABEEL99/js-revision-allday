a3e1622ecae489a1d5b7f3018b3f8f2cc9cf93c9
SALSABEEL99 <sabeelbaloch21@gmail.com>
2024-07-03 16:54:46 +0500

    loop revision topic

---
+++ script.js
@@ -0,0 +1,25 @@
+/* for loop print 1 to 5
+for( let count=1; count<=5; count++){
+    console.log("salsabeel");
+} 
+//calculate sum of 1 to 5 
+let sum = 0;
+for (let i = 1; i <= 5; i++){
+    sum = sum + i;//sum = 15
+}
+    console.log('sum =',sum );
+    */
+   // infinite loooop never do in program
+   //while loop 
+   //let i =1;
+   //while (i<=7) {
+    //console.log('salsabeel');
+    //i++
+   //}
+  // let i = 1;
+  // do {
+   // console.log("sabeel",i);
+  //  i++;
+//  } while (i <= 5);
+
+// for of loop 
