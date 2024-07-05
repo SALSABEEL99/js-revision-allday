4a4535544d66edbb3aed1b174d156e8bdb7c7c47
SALSABEEL99 <sabeelbaloch21@gmail.com>
2024-07-06 00:50:25 +0500

    loop practise done with practise

--- script.js
+++ script.js
@@ -9,17 +9,63 @@ for (let i = 1; i <= 5; i++){
 }
     console.log('sum =',sum );
     */
-   // infinite loooop never do in program
-   //while loop 
-   //let i =1;
-   //while (i<=7) {
-    //console.log('salsabeel');
-    //i++
-   //}
-  // let i = 1;
-  // do {
-   // console.log("sabeel",i);
-  //  i++;
+// infinite loooop never do in program
+//while loop
+//let i =1;
+//while (i<=7) {
+//console.log('salsabeel');
+//i++
+//}
+// let i = 1;
+// do {
+// console.log("sabeel",i);
+//  i++;
 //  } while (i <= 5);
 
-// for of loop 
+// for of loop & for in
+// for of help for string & arrays
+// for off loop
+//let str = 'sabeel';
+
+//let length = 0; //calculate length
+//for(let i of str ) {// isse iterator b boltehe
+//console.log('i=', i);//all values printed in console 's a b e e l'
+//  length++;
+//}
+
+//console.log('string length', length);// lenght is = 6
+////
+// for in looop use for object 7 arrays'
+
+//let student = {
+//  name : 'sabeel',
+//age : 20,
+//batch : 11,
+//ispass : true
+//};
+
+//for (let key in student ) {
+//console.log(key);// only key print
+//  console.log('key=',key,': value=',student[key]); // key and value is print
+//}
+
+// practise Question
+// print all  even number from 0 to 100;
+//for (let i = 0; i <= 100; i++) {
+  //console.log(i);}// done loop working from 0 to 100;
+
+  //if (i % 2 === 0) {
+    // only print even number;
+   // console.log(i);
+ // }
+//}
+//practise question 2 
+// collect correct number with user 
+//let gameNum = 25;
+
+//let userNum = prompt('guess the correct number');
+//while (userNum != gameNum) {//game
+
+  //userNum = prompt('you enter wrong number try again')
+//}
+//console.log('congrats you enter correct no');// its working 
